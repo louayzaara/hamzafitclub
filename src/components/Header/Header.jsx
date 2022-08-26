a07fe7f287f5d19dfa1363f3e033fcc7bc625db6
unknown <louaybenzaara@outlook.fr>
2022-08-26 15:21:37 +0100

    last committtttee

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
 import Bars from '../../assets/bars.png'
-import {Link} from "react-scroll"
+import { Link } from "react-scroll"
 
 const Header = () => {
 
@@ -14,17 +14,17 @@ const Header = () => {
       <img src={Logo} alt="logo" className='logo' />
       {menuOpened === false && mobile === true ? (
         <div style={{ backgroundColor: "var(--appColor", padding: "0.5rem", borderRadius: "5px" }}
-        onClick={()=> setMenuOpened(true)}
+          onClick={() => setMenuOpened(true)}
         >
           <img src={Bars} alt="" style={{ width: '1.5rem', height: '1.5rem' }} />
         </div>
       ) : (
         <ul className='header-menu'>
-          <li><Link onClick={()=>setMenuOpened(false)} activeClass='active' to='Home' spy={true} smooth={true}>Home</Link></li>
-          <li><Link onClick={()=>setMenuOpened(false)} to='Programs' spy={true} smooth={true}>Programs</Link></li>
-          <li><Link onClick={()=>setMenuOpened(false)} to='reasons' spy={true} smooth={true}>Why me</Link></li>
-          <li><Link onClick={()=>setMenuOpened(false)} to='plans' spy={true} smooth={true}>Plans</Link></li>
-          <li><Link onClick={()=>setMenuOpened(false)} to='Testimonials' spy={true} smooth={true}>Testimonials</Link></li>
+          <li><Link onClick={() => setMenuOpened(false)} activeClass='active' to='Home' spy={true} smooth={true}>Home</Link></li>
+          <li><Link onClick={() => setMenuOpened(false)} to='Programs' spy={true} smooth={true}>Programs</Link></li>
+          <li><Link onClick={() => setMenuOpened(false)} to='reasons' spy={true} smooth={true}>Why me</Link></li>
+          <li><Link onClick={() => setMenuOpened(false)} to='plans' spy={true} smooth={true}>Plans</Link></li>
+          <li><Link onClick={() => setMenuOpened(false)} to='Testimonials' spy={true} smooth={true}>Testimonials</Link></li>
         </ul>
       )}
     </div>
